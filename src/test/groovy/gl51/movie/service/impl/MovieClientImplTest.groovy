dbf4064a15c08bd1ee11a619e2b755dec5081b97
ASaunier <saunier.aurelien.mail@gmail.com>
2020-04-02 18:22:40 +0200

    Add MovieClientImplTest

---
+++ src/test/groovy/gl51/movie/service/impl/MovieClientImplTest.groovy
@@ -0,0 +1,37 @@
+package gl51.movie.service.impl
+
+import gl51.movie.data.Movie
+import gl51.movie.service.MovieClient
+import io.micronaut.test.annotation.MicronautTest
+
+import spock.lang.Specification
+
+import javax.inject.Inject
+
+@MicronautTest
+class MovieClientImplTest extends Specification {
+
+    @Inject
+    MovieClient movieClientMock = Mock()
+
+    @Inject
+    MovieRegistryImpl registry
+
+    void "Injection should work"(){
+        expect:
+        movieClientMock != null
+        registry != null
+    }
+
+    void "Getting a movie by it's imdb ID should work"(){
+        when:
+            movieClientMock.getMovieDetail("1234abcd")
+
+        then:
+        Movie movie = new Movie(imdbId: "1234abcd")
+        registry.addMovieToFavorites(movie.imdbId)
+        registry.listFavorites().size() == 1
+
+    }
+
+}
