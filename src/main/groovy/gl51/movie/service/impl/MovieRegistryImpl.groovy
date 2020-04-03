622467a6bf83c68bb2214e44d775864835215a87
ASaunier <saunier.aurelien.mail@gmail.com>
2020-04-03 16:31:27 +0200

    Add MovieClient mock

--- src/main/groovy/gl51/movie/service/impl/MovieRegistryImpl.groovy
+++ src/main/groovy/gl51/movie/service/impl/MovieRegistryImpl.groovy
@@ -1,19 +1,23 @@
 package gl51.movie.service.impl
 
 import gl51.movie.data.Movie
+import gl51.movie.service.MovieClient
 import gl51.movie.service.MovieRegistry
 
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MovieRegistryImpl implements MovieRegistry {
 
     private List<Movie> internalRegistry = []
+    @Inject
+    private MovieClient movieClient
 
     @Override
     void addMovieToFavorites(String imdbID) {
         // << = .add
-        internalRegistry << new Movie(imdbId: imdbID)
+        internalRegistry << movieClient.getMovieDetail(imdbID)
     }
 
     @Override
--- src/test/groovy/gl51/movie/service/impl/MovieRegistryImplTest.groovy
+++ src/test/groovy/gl51/movie/service/impl/MovieRegistryImplTest.groovy
@@ -1,6 +1,9 @@
 package gl51.movie.service.impl
 
+import gl51.movie.data.Movie
+import gl51.movie.service.MovieClient
 import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.annotation.MockBean
 import spock.lang.Specification
 
 import javax.inject.Inject
@@ -23,8 +26,16 @@ class MovieRegistryImplTest extends Specification {
 
     void "adding a favorite should populate the database"(){
         when:
-            registry.addMovieToFavorites("aaaaa")
+            registry.addMovieToFavorites("aaaa")
         then:
             registry.listFavorites().size() == 1
+            registry.listFavorites().find{it.title = "my movie"}
+    }
+
+    @MockBean(MovieClientImpl)
+    MovieClient movieClient() {
+        def mock = Mock(MovieClient)
+        mock.getMovieDetail("aaaa") >> new Movie(imdbId: "aaaa", title: "my movie")
+        mock
     }
 }
